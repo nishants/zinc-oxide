f8d4c6f09ae5c51925fa9b9989a2ba71d9fbd35d
nishant <nishant.singh87@gmail.com>
2017-04-11 19:51:45 +0530

    view vocab page.

--- src/app/routes.js
+++ src/app/routes.js
@@ -11,8 +11,18 @@ angular.module("zinc").config(["$stateProvider", "$urlRouterProvider", "$locatio
 				templateUrl: 'assets/templates/vocab.html'
 			})
 			.state('vocab.view', {
-				url: '/view',
-				templateUrl: 'assets/templates/vocab-view.html'
+				url: '/:id/view',
+				templateUrl: 'assets/templates/vocab-view.html',
+				resolve: {
+					vocabset: [
+						"$stateParams",
+						function($stateParams){
+							return {id: $stateParams.id,title: "Fun Words", by: "Zinc", description: "Fun Words to learn this summer.", words: "one, two, three, four", status: "bonus", time: 1212, assigned: {by : {name: 'Tamara Oslon', image: 'assets/images/teacher.jpg'} , on:'Dec 12 2016'}};
+						}
+					]				},
+				controller: ["$scope", "vocabset", function($scope, vocabset){
+					$scope.vocabset = vocabset;
+				}]
 			});
 
 	$locationProvider.html5Mode({
