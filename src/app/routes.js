e8fd62c92854b957cc571ad692a2f2e5b7773521
nishant <nishant.singh87@gmail.com>
2017-04-11 16:35:47 +0530

    render states by route.

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,10 +1,13 @@
 angular.module("zinc").config(["$stateProvider", "$urlRouterProvider", "$locationProvider",function($stateProvider, $urlRouterProvider, $locationProvider){
 
-	$urlRouterProvider.otherwise('/home');
+	$urlRouterProvider.otherwise('/vocab');
 	$stateProvider
 			.state('home', {
 				url: '/home',
 				templateUrl: 'assets/templates/home.html'
+			}).state('vocab', {
+				url: '/vocab',
+				templateUrl: 'assets/templates/vocab.html'
 			});
 
 	$locationProvider.html5Mode({
