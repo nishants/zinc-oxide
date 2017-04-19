cb866bc66446672eb3dc12e8724e07f2d4fbad51
nishant <nishant.singh87@gmail.com>
2017-04-19 17:17:41 +0530

    sample json for deck_participation.

--- src/app/game-play/game-play-service.js
+++ src/app/game-play/game-play-service.js
@@ -1,9 +1,20 @@
 angular.module("zinc").service("GamePlayService", ["$http", function ($http) {
+	var toWord = function(data){
+		return {
+			name: data.word,
+			audio: data.pronunciation_audio,
+			synonym: {
+				options: data.synonym_option_names,
+				correct : data.correct_synonym
+			}
+		};
+	};
 	return {
 		create: function(data){
 			return {
-				title: data.title,
-				image: data.image,
+				title: data.deck.set_title,
+				image: data.deck.set_image_url,
+				words: data.deck.words.map(toWord)
 			};
 		}
 	}
