d25264d08016a263d65a61879193b99c8276043e
nishant <nishant.singh87@gmail.com>
2017-05-20 21:30:12 +0530

    show intro state.

--- src/app/skills/crgameplay/player/crg-player-service.js
+++ src/app/skills/crgameplay/player/crg-player-service.js
@@ -4,11 +4,16 @@ app.factory("CRGPlayer", ['GameIntroState', function (GameIntroState) {
       var player = {
         points: 10,
         sound: true,
-        state: GameIntroState(),
+        transitTo: function(state){
+          player.state = state;
+        },
+        state: null,
         exit: function () {
           window.location = "#";
         },
+
       };
+      player.state = GameIntroState(player);
       return player;
     }
   };
--- src/app/skills/crgameplay/player/states/game-intro-state.js
+++ src/app/skills/crgameplay/player/states/game-intro-state.js
@@ -1,15 +1,41 @@
 app.factory("GameIntroState", [function () {
-  return function(){
-    return {
+  return function(player){
+    var showInput = function(state){
+      state.showInput = true;
+      state.buttons   = [];
+      state.submitInput = function(userInput){
+        console.log("user says : " + userInput);
+        player.transitTo({});
+      }
+    };
+
+    var state = {
       showInput: false,
       buttons: [
         {
           label: "I am done!",
-          onClick : function(){
-
+          onClick: function () {
+            state.buttons = [
+              {
+                label: "Definitely",
+                onClick: function () {
+                  console.log("Definitely");
+                  showInput(state);
+                }}, {
+                label: "Sort of",
+                onClick: function () {
+                  console.log("Sort of");
+                  showInput(state);
+                }}, {
+                label: "Not Really",
+                onClick: function () {
+                  console.log("Not Really");
+                  showInput(state);
+                }}]
           }
         }
       ]
     };
+    return state;
   };
 }]);
