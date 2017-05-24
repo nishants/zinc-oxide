70e37598636aefbc3817a8db4f6c8abd2de4350f
nishant <nishant.singh87@gmail.com>
2017-05-25 02:12:47 +0530

    gameplan from route.

--- src/app/routes.js
+++ src/app/routes.js
@@ -21,7 +21,47 @@ angular.module("zinc").config(["$stateProvider", "$urlRouterProvider", "$locatio
 			.state('crg.gameplay', {
 				url: '/gameplay',
 				templateUrl: 'assets/templates/crg-gameplay-template.html',
-        controller: 'CRGameplayController'
+        controller: 'CRGameplayController',
+        resolve : {
+          gamePlan: [function(){
+            return {
+              zincing: {
+                visualize: [
+                  {
+                    phrase: [0, 1,2,3,4],
+                    transcript: 'What do you imagine when you read the text "Ships at a distance"  in first sentence ?'
+                  },
+                  {phrase: [34,35,36,37,38,39]},
+                ],
+                imagine: [
+                  {
+                    phrase: [57,58,59,60,61,62,63,64,65],
+                    existsInPassage: false,
+                    transcript: 'Which one of following is not a good example of "all those things they don\'t want to remember" ?',
+                    usages: [
+                      {
+                        label: 'A cow eating grass.',
+                        correct: false,
+                      },
+                      {
+                        label: 'A baby being dropped.',
+                        correct: true,
+                      },
+                      {
+                        label: 'Getting fired from job.',
+                        correct: true,
+                      },
+                      {
+                        label: 'A scary nightmare.',
+                        correct: true,
+                      },
+                    ]
+                  }
+                ]
+              }
+            };
+          }]
+        }
 			})
       .state('crg.editor', {
         url: '/editor',
@@ -31,7 +71,13 @@ angular.module("zinc").config(["$stateProvider", "$urlRouterProvider", "$locatio
       .state('crg.editor.preview', {
         url: '/preview',
         templateUrl: 'assets/templates/crg-preview-template.html',
-        controller: 'CRGameplayController'
+        controller: 'CRGameplayController',
+        resolve : {
+          gamePlan: ['CRGEditorService', function(CRGEditorService){
+            return CRGEditorService.prepareGamePlan();
+          }]
+        }
+
       })
 			.state('vocab', {
 				url: '/vocab',
