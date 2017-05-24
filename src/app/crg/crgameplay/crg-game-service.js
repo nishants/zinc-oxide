86e42a03bf73b3914555e7eaa1ec8ec20903e9e8
nishant <nishant.singh87@gmail.com>
2017-05-24 19:26:21 +0530

    CRG Editor skill form bar.

--- src/app/crg/crgameplay/crg-game-service.js
+++ src/app/crg/crgameplay/crg-game-service.js
@@ -1,6 +1,6 @@
 app.factory("CRGService", [function () {
   var paragraphs = [
-        "Ships at distance have every man's wish on board. For some they come in with the tide. For others they sail forever on the horizon, never out of sight, never landing until the watcher turn his eyes away in resignation, his dreams mocked to death by time. That is the life of men.",
+        "Ships at a distance have every man's wish on board. For some they come in with the tide. For others they sail forever on the horizon, never out of sight, never landing until the watcher turn his eyes away in resignation, his dreams mocked to death by time. That is the life of men.",
         "Now, women forget all those things they don't  want to remember, and remember everything they don't want to forget. The dream is the truth. Then they act and do things accordingly."
       ],
       toSelectableNodes = function(paragraphs){
