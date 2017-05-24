42c9c590b1d834156a1a9b494d9e7d9f78c4a7f6
nishant <nishant.singh87@gmail.com>
2017-05-25 01:50:55 +0530

    Gameplan from route.

--- src/app/crg/crgameplay/crg-controller.js
+++ src/app/crg/crgameplay/crg-controller.js
@@ -1,6 +1,42 @@
 app.controller('CRGameplayController', ['$scope', '$timeout', 'CRGPlayer', 'CRGService', function ($scope, $timeout, CRGPlayer, CRGService) {
 
-  var game    = CRGService.create();
+  var gamePlan = {
+    zincing: {
+      visualize: [
+        {
+          phrase: [0, 1,2,3,4],
+          transcript: 'What do you imagine when you read the text "Ships at a distance"  in first sentence ?'
+        },
+        {phrase: [34,35,36,37,38,39]},
+      ],
+      imagine: [
+        {
+          phrase: [57,58,59,60,61,62,63,64,65],
+          existsInPassage: false,
+          transcript: 'Which one of following is not a good example of "all those things they don\'t want to remember" ?',
+          usages: [
+            {
+              label: 'A cow eating grass.',
+              correct: false,
+            },
+            {
+              label: 'A baby being dropped.',
+              correct: true,
+            },
+            {
+              label: 'Getting fired from job.',
+              correct: true,
+            },
+            {
+              label: 'A scary nightmare.',
+              correct: true,
+            },
+          ]
+        }
+      ]
+    }
+  };
+  var game    = CRGService.create(gamePlan);
   game.player = CRGPlayer.create(game);
 
   $scope.onTextSelect = function(indexes){
--- src/app/crg/crgameplay/crg-game-service.js
+++ src/app/crg/crgameplay/crg-game-service.js
@@ -16,44 +16,9 @@ app.factory("CRGService", [function () {
       };
 
   var service = {
-    create: function(game){
+    create: function(gamePlan){
       var game = {
-        plan: {
-          zincing: {
-            visualize: [
-              {
-                phrase: [0, 1,2,3,4],
-                transcript: 'What do you imagine when you read the text "Ships at a distance"  in first sentence ?'
-              },
-              {phrase: [34,35,36,37,38,39]},
-            ],
-            imagine: [
-              {
-                phrase: [57,58,59,60,61,62,63,64,65],
-                existsInPassage: false,
-                transcript: 'Which one of following is not a good example of "all those things they don\'t want to remember" ?',
-                usages: [
-                  {
-                    label: 'A cow eating grass.',
-                    correct: false,
-                  },
-                  {
-                    label: 'A baby being dropped.',
-                    correct: true,
-                  },
-                  {
-                    label: 'Getting fired from job.',
-                    correct: true,
-                  },
-                  {
-                    label: 'A scary nightmare.',
-                    correct: true,
-                  },
-                ]
-              }
-            ]
-          }
-        },
+        plan: gamePlan,
         from: "Their Eyes Were Watching God",
         by: "Zora Neal Hurston",
         passage: toSelectableNodes(paragraphs),
