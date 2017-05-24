551ca92ab04f0ee6816755891b41eb59206e51cf
nishant <nishant.singh87@gmail.com>
2017-05-24 22:00:58 +0530

    Skill section styling.

--- src/app/crg/editor/crg-editor-controller.js
+++ src/app/crg/editor/crg-editor-controller.js
@@ -1,10 +1,22 @@
 app.controller('CRGEditorController', ['$scope', '$timeout', 'CRGEditorService', function ($scope, $timeout, CRGEditorService) {
 
+
+
+  var editor  = CRGEditorService;
+  var passage = {
+    textSelected: function(){
+
+    },
+    select: function(params){
+      var args = params.sceneName || params.selectFocus || params.selectPhrase;
+    }
+  };
+
   $scope.onTextSelect = function(indexes){
     $timeout(function(){
     });
   };
 
-  var editor  = CRGEditorService;
-
+  $scope.editor = editor;
+  $scope.passage = passage;
 }]);
