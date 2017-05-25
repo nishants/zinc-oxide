c8e50a77821b9eb5098f74f00765088d3fc0503d
nishant <nishant.singh87@gmail.com>
2017-05-25 15:50:32 +0530

    A sample game plan.

--- src/app/variables.js
+++ src/app/variables.js
@@ -1,4 +1,43 @@
 app.value("stateMessages", {
-	"vocab.view"  : "Opening Vocabulary Set",
-	"default"     : "Loading ",
+  "vocab.view"  : "Opening Vocabulary Set",
+  "default"     : "Loading ",
+});
+
+app.value("SampleCRGGamePlan", {
+  zincing: {
+    visualize: [
+      {
+        phrase: {indices: [0, 1,2,3,4]},
+        transcript: {text: 'What do you imagine when you read the text "Ships at a distance"  in first sentence ?'}
+      },
+      {phrase: {indices: [34,35,36,37,38,39]} ,
+        transcript: {text: 'What do you imagine when you read the text "looks aways in resignation"  in first sentence ?'}
+      },
+    ],
+    imagine: [
+      {
+        phrase: {indices: [57,58,59,60,61,62,63,64,65]},
+        existsInPassage: false,
+        transcript: {text: 'Which one of following is not a good example of "all those things they don\'t want to remember" ?'},
+        usages: [
+          {
+            label: 'A cow eating grass.',
+            correct: false,
+          },
+          {
+            label: 'A baby being dropped.',
+            correct: true,
+          },
+          {
+            label: 'Getting fired from job.',
+            correct: true,
+          },
+          {
+            label: 'A scary nightmare.',
+            correct: true,
+          },
+        ]
+      }
+    ]
+  }
 });
