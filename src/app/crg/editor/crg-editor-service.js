319517c3790fa2a5064795e3c65b5ec18cf6633d
nishant <nishant.singh87@gmail.com>
2017-05-25 13:45:33 +0530

    Set skills on editor.game.

--- src/app/crg/editor/crg-editor-service.js
+++ src/app/crg/editor/crg-editor-service.js
@@ -5,40 +5,57 @@ app.factory("CRGEditorService", ["Passage", "PassageSelector", function (Passage
           zincing: {
             visualize: [
               {
-                phrase: [0, 1,2,3,4],
+                phrase: {
+                  indices: [0, 1,2,3,4],
+                  text : 'Ships at a distance',
+                },
+                focus: {
+                  indices: [0, 1,2,3,4,5,6,7,8,9],
+                  text :"Ships at a distance have every man's wish on board",
+                },
                 transcript: 'What do you imagine when you read the text "Ships at a distance"  in first sentence ?'
-              },
-              {phrase: [34,35,36,37,38,39]},
-            ],
-            imagine: [
-              {
-                phrase: [57,58,59,60,61,62,63,64,65],
-                existsInPassage: false,
-                transcript: 'Which one of following is not a good example of "all those things they don\'t want to remember" ?',
-                usages: [
-                  {
-                    label: 'A cow eating grass.',
-                    correct: false,
-                  },
-                  {
-                    label: 'A baby being dropped.',
-                    correct: true,
-                  },
-                  {
-                    label: 'Getting fired from job.',
-                    correct: true,
-                  },
-                  {
-                    label: 'A scary nightmare.',
-                    correct: true,
-                  },
-                ]
               }
-            ]
+            ],
+            imagine: []
           }
         },
         prepareGamePlan : function(){
-          return editorService.game;
+          return {
+            zincing: {
+              visualize: [
+                {
+                  phrase: [0, 1,2,3,4],
+                  transcript: 'What do you imagine when you read the text "Ships at a distance"  in first sentence ?'
+                },
+                {phrase: [34,35,36,37,38,39]},
+              ],
+              imagine: [
+                {
+                  phrase: [57,58,59,60,61,62,63,64,65],
+                  existsInPassage: false,
+                  transcript: 'Which one of following is not a good example of "all those things they don\'t want to remember" ?',
+                  usages: [
+                    {
+                      label: 'A cow eating grass.',
+                      correct: false,
+                    },
+                    {
+                      label: 'A baby being dropped.',
+                      correct: true,
+                    },
+                    {
+                      label: 'Getting fired from job.',
+                      correct: true,
+                    },
+                    {
+                      label: 'A scary nightmare.',
+                      correct: true,
+                    },
+                  ]
+                }
+              ]
+            }
+          };
         },
     passageSelector: PassageSelector
   };
--- src/app/crg/editor/form/zincing/zinc-visualize-editor-service.js
+++ src/app/crg/editor/form/zincing/zinc-visualize-editor-service.js
@@ -1,6 +1,6 @@
 app.service("ZincVisualizeEditorService", ['$timeout', 'CRGEditorService', function ($timeout, CRGEditorService) {
 
-  var defaultTranscript = 'What do you imagine when you read the phrase "<phrase>"?',
+  var defaultTranscript = 'What do you visualize when you read the phrase "<phrase>"?',
       zincVisualizeEditor = {
         list: [],
         remove: function(index){
@@ -11,7 +11,7 @@ app.service("ZincVisualizeEditorService", ['$timeout', 'CRGEditorService', funct
             focus: true,
             phrase: true,
             whenDone: function(selection){
-              zincVisualizeEditor.list.push({
+              CRGEditorService.game.zincing.visualize.push({
                 focus: selection.focus,
                 phrase: selection.phrase,
                 transcript: {text: defaultTranscript.replace("<phrase>", selection.phrase.text)},
