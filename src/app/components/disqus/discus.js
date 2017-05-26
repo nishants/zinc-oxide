8d09708c1746c7d76d015b121cb4f1ad63d3ad8e
nishant <nishant.singh87@gmail.com>
2017-05-26 14:57:58 +0530

    Adding disqus for comments.

---
+++ src/app/components/disqus/discus.js
@@ -0,0 +1,11 @@
+var disqus_config = function () {
+  this.page.url = window.location.href;  // Replace PAGE_URL with your page's canonical URL variable
+  this.page.identifier = "http://nishants.site/zinc/crg" ; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+  var d = document, s = d.createElement('script');
+  s.src = 'https://nishants-site-1.disqus.com/embed.js';
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
+})();
