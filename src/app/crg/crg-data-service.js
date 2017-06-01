c8886975acb76870f762d4a96387ea8330185a4b
nishant <nishant.singh87@gmail.com>
2017-06-01 17:00:31 +0530

    Styling editor form.

--- src/app/crg/crg-data-service.js
+++ src/app/crg/crg-data-service.js
@@ -1,8 +1,17 @@
 app.factory("CRGDataService", ["$http",function ($http) {
+  var
+      labelFor = function(scene){
+        return scene.name.replace(/-/g, " ");
+      },
+      setScene = function(scene){
+        scene.label = labelFor(scene)
+        return scene;
+      };
   return {
     getGame: function(id){
       var url = "assets/data/crg-sample-game-data-<id>.json".replace("<id>", id);
       return $http.get(url).then(function(response){
+        response.data.script.scenes = response.data.script.scenes.map(setScene);
         return response.data;
       })
     }
