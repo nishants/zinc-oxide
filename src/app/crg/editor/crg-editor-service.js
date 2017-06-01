cf099f0bde40f1fc119b4427bceb9bf142d18782
nishant <nishant.singh87@gmail.com>
2017-06-01 16:15:24 +0530

    Ordered scenes in editor.

--- src/app/crg/editor/crg-editor-service.js
+++ src/app/crg/editor/crg-editor-service.js
@@ -1,23 +1,29 @@
 app.factory("CRGEditorService", ["Passage", "PassageSelector", function (Passage, PassageSelector) {
   var editorService = {
-        game: {
-          zincing: {
-            imagine : [],
-            visualize : [],
-          }
+        script: {
+          scenes: []
+        },
+        passage: {
+          to: null,
+          from: null,
+          text: null,
         },
         setGameToEdit: function(gameData){
-          editorService.game = gameData;
-          editorService.passageSelector =  PassageSelector(Passage(gameData.passage));
+          editorService.script.scenes   = gameData.script.scenes;
+          editorService.passage         = gameData.passage;
+          editorService.passageSelector =  PassageSelector(Passage(editorService.passage));
         },
         prepareGamePlan : function(){
-          var gameData = JSON.parse(JSON.stringify(editorService.game));
-          gameData.passge ={
-            from: "from book",
-            by  : "by author",
-            text: "this is the text",
-          };
+          var gameData = JSON.parse(JSON.stringify({
+            passage : editorService.passage,
+            script  : editorService.script
+          }));
           return gameData;
+        },
+        removeScene: function(sceneToRemove){
+          editorService.script.scenes = editorService.script.scenes.filter(function(scene){
+            return sceneToRemove !== scene;
+          });
         },
        passageSelector: null
   };
