aa4dd99f0691d0f446583b983efd32ce1335a051
nishant <nishant.singh87@gmail.com>
2017-06-04 14:16:53 +0530

    Add data API to crg home.

--- src/app/crg/crg-data-service.js
+++ src/app/crg/crg-data-service.js
@@ -9,7 +9,7 @@ app.factory("CRGDataService", ["$http",function ($http) {
       };
   return {
     getGame: function(id){
-      var url = "assets/data/crg-sample-game-data-<id>.json".replace("<id>", id);
+      var url = "assets/data/crg/crg-sample-game-data-<id>.json".replace("<id>", id);
       return $http.get(url).then(function(response){
         response.data.script.scenes = response.data.script.scenes.map(setScene);
         return response.data;
