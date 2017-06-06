c82e77cec7d0c535e1d6ed7ac35bb30fd3171882
nishant <nishant.singh87@gmail.com>
2017-06-07 05:18:32 +0530

    scene form styling : edit field.

--- src/app/crg/scenes/multi-choice/multi-choice-editor.js
+++ src/app/crg/scenes/multi-choice/multi-choice-editor.js
@@ -1,10 +1,11 @@
-app.service("MultiChoiceEditor", ['$timeout', 'CRGEditorService', function ($timeout, CRGEditorService) {
+app.service("MultiChoiceEditor", ['SceneLoader', function (SceneLoader) {
 
   var multiChoiceEditor = {
         createFor: function(group){
           return             {
             "group"       : group,
             "name"        : "multi-choice",
+            label         : SceneLoader["multi-choice"].label,
             "config"      : {
               "phrase": {"indices": [], "text"  : ""},
               "focus": {"indices" : [], "text"  : ""},
